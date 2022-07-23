cce9dc91034a95c17b28d10d2775f83def845d32
Andrew Kane <andrew.kane@gmail.com>
2022-07-23 15:49:04 -0500

    Added monthly cost background color and table row removal functionality

--- client.js
+++ client.js
@@ -1,20 +1,22 @@
 console.log('js');
 $(document).ready(onReady);
+let monthTotal = 0;
 
 function onReady() {
     console.log('DOM ready.');
     $('#submit').on('click', entable);
+    $('#empTableBody').on('click', 'button', removeEmp); //I was trying without success to get this to point at the delete button's class. I replaced it with "button" as a shot in the dark and it worked.
 }
 
 function entable() {
-    // console.log('Submit pressed.')
+    console.log('Submit pressed.')
     $('#warning').empty();
-    let fName; let lName; let id; let title; let annualSalary;
+    let fName; let lName; let eid; let title; let annualSalary;
 
     // Drop input values into js variables
     fName = $('#fNameIn').val();
     lName = $('#lNameIn').val();
-    id = $('#idIn').val();
+    eid = $('#idIn').val();
     title = $('#titleIn').val();
     annualSalary = $('#annualSalaryIn').val();
 
@@ -27,7 +29,7 @@ function entable() {
         $('#warning').append('All fields are required.');
         return false;
     }
-    if (id.length < 1) {
+    if (eid.length < 1) {
         $('#warning').append('All fields are required.');
         return false;
     }
@@ -44,25 +46,51 @@ function entable() {
 
     // Drop input data into the table
     $('#empTableBody').append(`
-    <tr>
+    <tr class="` + String(eid) + `">
         <td>` + fName + `</td>
         <td>` + lName + `</td>
-        <td>` + id + `</td>
+        <td>` + eid + `</td>
         <td>` + title + `</td>
-        <td>` + annualSalary + `</td>
+        <td class="${String(eid)}">` + annualSalary + `</td>
+        <td> <button class="${String(eid)}">Delete</button> </td>
     </tr> `);
 
     // Clear input fields
-    $('#fNameIn').val('');
-    $('#lNameIn').val('');
-    $('#idIn').val('');
-    $('#titleIn').val('');
-    $('#annualSalaryIn').val('');
+    // $('#fNameIn').val('');
+    // $('#lNameIn').val('');
+    // $('#idIn').val('');
+    // $('#titleIn').val('');
+    // $('#annualSalaryIn').val('');
 
     // Call monthly cost calc function
-    sumMonthly();
+    sumMonthly(annualSalary);
 }
 
-function sumMonthly() {
-    
+function sumMonthly(yearSal) {
+    let oldTotal = $('#totalMonthly').text();
+    // console.log(oldTotal);
+    let monthSal = Math.ceil(yearSal / 12);
+    let monthSum = Number(oldTotal) + Number(monthSal);
+    $('#totalMonthly').text(monthSum);
+    // $("h2").css("background-color", "blue");
+    // $("h2").css("font-style", "italic");
+    // console.log(Number(monthSum));
+    if (Number(monthSum) > 19999) {
+        // console.log(true)
+        $('#totalMonthly').css("background-color", "red"); // Took lots of troubleshooting to get this working. Not sure how I fixed it. Had something to do with removing a colon after "background-color" that I thought I had added partway through troubleshooting.
+    }
+    monthTotal = monthSum;
+    return monthTotal;
+}
+
+function removeEmp() {
+    // console.log('delete pushed');
+    let x = ($(this).attr('class'));
+    // console.log(x);
+    $('.' + x).remove();
+    let oldTotal = $('#totalMonthly').text();
+    let newTotal = oldTotal - monthTotal;
+    $('#totalMonthly').text(newTotal);
+    return monthTotal;
+
 }
